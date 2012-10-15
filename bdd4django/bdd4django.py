f78d57ed03d711e43c1d5416b3e4961f2ce7ebdb
Daniel França <daniel.franca@gmail.com>
2012-10-15 19:42:34 -0300

    See variable fieds of the step in the console

--- bdd4django/bdd4django.py
+++ bdd4django/bdd4django.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.0'
+__version__ = '0.3.2'
 
 #  TODO  get working with python 3,4,5, etc...
 #  TODO  put http://www.dawnoftimecomics.com/index.php on comixpedia!
